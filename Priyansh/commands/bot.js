f53bd91ced4f716c30ffbb5459d1008ae68ba9b7
skbrand1122 <babykumarigst1@gmail.com>
2025-08-13 22:32:36 +0530

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "goibot",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "Fixed By Arun Kumar",
+  credits: "Fixed By Sultan Mirza",
   description: "goibot",
   commandCategory: "Noprefix",
   usages: "noprefix",
